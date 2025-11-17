529df56d42bc0db5ea86faefe8cd684f7e7fcbb6
NA-Sugoi <ducna.pc19@gmail.com>
2025-11-17 16:12:30 +0700

    fix

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -1,32 +1,34 @@
 // /services/socketService.js
-import { Server } from 'socket.io';
-import { jwtAuthMiddleware } from '../middlewares/authMiddleware.js';
-import  * as roomController from '../controllers/roomController.js';
+import { Server } from "socket.io";
+import { jwtAuthMiddleware } from "../middlewares/authMiddleware.js";
+import * as roomController from "../controllers/roomController.js";
+import dotenv from "dotenv";
+dotenv.config();
 const users = new Map();
 const initSocketIO = (httpServer) => {
-    const io = new Server(httpServer, {
-        cors: {
-            origin: "*", // Cần thay bằng domain của FE
-            methods: ["GET", "POST"]
-        },
-        transports: ["websocket", "polling"],
-        allowEIO3: true,
-    });
-io.use(jwtAuthMiddleware);
-io.on("connection", (socket) => {
-  console.log(" Client connected:", socket.id);
-  socket.io = io;
-  socket.on('joinRoom', (data) => roomController.joinRoom(socket, data));
-  socket.on('leaveRoom', (data) => roomController.leaveRoom(socket, data));
-  socket.on('disconnect', () => roomController.disconnect(socket,io));
-
-  // Handle errors
-  socket.on("error", (error) => {
-    console.error("Socket error:", error);
+  const io = new Server(httpServer, {
+    cors: {
+      origin: process.env.FRONTEND_URL,
+      methods: ["GET", "POST"],
+    },
+    transports: ["websocket"],
+    allowEIO3: true,
   });
-});
+  io.use(jwtAuthMiddleware);
+  io.on("connection", (socket) => {
+    console.log(" Client connected:", socket.id);
+    socket.io = io;
+    socket.on("joinRoom", (data) => roomController.joinRoom(socket, data));
+    socket.on("leaveRoom", (data) => roomController.leaveRoom(socket, data));
+    socket.on("disconnect", () => roomController.disconnect(socket, io));
 
-    return io;
+    // Handle errors
+    socket.on("error", (error) => {
+      console.error("Socket error:", error);
+    });
+  });
+
+  return io;
 };
 
 export { initSocketIO };
